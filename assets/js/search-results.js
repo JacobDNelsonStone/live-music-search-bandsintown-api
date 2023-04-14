ec4e0913c07ecda399feb9b045b6e5817f3f970e
Ian Bui <thinkbui@gmail.com>
2023-04-14 12:21:16 -0500

    Removed timeframe decoder draft

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -20,30 +20,11 @@ if(qParam){
   console.log(url.href);
   url.searchParams.set("q", qParam);
   console.log(url.href);
-
-    // If timeframeParam present, convert to SerpAPI format and add to url
-    // date:today - Today's Events
-    // date:tomorrow - Tomorrow's Events
-    // date:week - This Week's Events
-    // date:today - Today's Weekend's Events
-    // date:next_week - Next Week's Events
-    // date:month - This Month's Events
-    // date:next_month - Next Month's Events
-
-  switch(timeframeParam){
-    case "today":
-      url.searchParams.set("timeframe", "date:today");
-      break;
-    case "tomorrow":
-      url.searchParams.set("timeframe", "date:tomorrow");
-      break;
-    case "week":
-      url.searchParams.set("timeframe", "date:week");
-      break;
-    default:
-      // noop
+  if(timeframeParam){
+    url.searchParams.set("timeframe", timeframeParam);
+    console.log(url.href);
   }
-  console.log(url.href);
+
 
   // Make API call
   // Parse result
